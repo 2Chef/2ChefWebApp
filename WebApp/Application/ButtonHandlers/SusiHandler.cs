8ab05b24d1b6f78ff075c06ad4b1457c99c0090b
buynik <bubanik123@yandex.ru>
2025-02-01 22:00:39 +0700

    Разработал кнопку SUSI

---
+++ WebApp/Application/ButtonHandlers/SusiHandler.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using WebApp.Kernel.ButtonHandlerReg;
+
+namespace WebApp.Application.ButtonHandlers
+{
+    [ButtonHandler("susi")]
+    internal sealed class SusiHandler : IButtonHandler
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public SusiHandler(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        public async Task Execute(CallbackQuery callbackData)
+        {
+            await _telegramClient.SendMessage(callbackData!.From!.Id, "🍣🍱🍤🥢🥡");
+        }
+    }
+}
--- WebApp/Application/ChatCommands/StartCommand.cs
+++ WebApp/Application/ChatCommands/StartCommand.cs
@@ -22,7 +22,7 @@ namespace WebApp.Application.ChatCommands
             InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
             [
                 [ InlineKeyboardButton.WithCallbackData("✅ Войти", "login"),  InlineKeyboardButton.WithCallbackData("🔢 Скалькулировать", "calculate") ],
-                [ InlineKeyboardButton.WithUrl("🌐 Открыть сайт", "https://www.youtube.com/watch?v=mDFBTdToRmw") ],
+                [ InlineKeyboardButton.WithUrl("🌐 Открыть сайт", "https://www.youtube.com/watch?v=mDFBTdToRmw") , InlineKeyboardButton.WithCallbackData("🍣 SUSI", "susi") ]
             ]);
 
             ChatId chatId = new ChatId(message.From.Id);
