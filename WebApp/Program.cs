6a886d9c9b103142b9f2de97cac4bef0b333f352
daskt <dasktelor@mail.ru>
2025-02-07 20:35:48 +0700

    Убрал appsettings.Development.json

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -21,7 +21,6 @@ namespace WebApp
                 .ConfigureAppConfiguration((context, config) =>
                 {
                     config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                          .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
                           .AddEnvironmentVariables()
                           .AddUserSecrets<Program>()
                           .AddCommandLine(args);
